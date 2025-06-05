cf6badfe28824271f9cea8184f97e71e4f36543f
rakesh <rakeshreddy@gmail.com>
2025-06-05 21:52:34 +0200

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -7,4 +7,16 @@ then
 echo "user is not root user"
 exit 23
 fi
+validate(){
+    if [ $1 -ne 0 ]
+    then
+    echo " $2 installation.... failure"
+    exit 1s
+    else
+    echo " $2 installation .... success"
+
+}
 yum install mysql -y
+validate $? mysql
+yum install postfix -y
+validate $? postfix
