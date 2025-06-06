72902eee68a4e9a5583bb500ea980046a2eb071a
rakesh <rakeshreddy@gmail.com>
2025-06-06 14:58:28 +0200

    forloop.sh

--- forloop.sh
+++ forloop.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-number=$1
-for i in {1..$number}
+DATE=$(date +%F-%H-%M-%S)
+filename=$0
+LOGFILE=/tmp/$filename-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+for i in $@
 do
+$i --version
+if [ $? -ne 0 ]
+then
 
-echo $i
+yum install $i -y &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+echo -e "installation of $i .......$R failure $N"
+exit 1
+else
+echo -e "installation of $i .......$G success $N"
+fi
+else
+echo -e " $i ....... $Y already installed $N"
+fi 
+
+
 done
