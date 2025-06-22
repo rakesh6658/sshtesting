b63aac47bb1df61e9f5ffb18c5f3b58033c9363e
rakesh <rakeshreddy@gmail.com>
2025-06-22 13:37:15 +0200

    added deletinglogs.sh

---
+++ deletinglogs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+app-logs=/home/ec2-user/app-logs
+DATE=$(date +%F-%H-%M-%S)
+scriptname=$0
+LOGFILE=$LOGS_DIR/$scriptname-$DATE.log
+
+FILES_TO_DELETE= $( find -name "*.log" -mtime +14)
+echo "$FILES_TO_DELETE"
