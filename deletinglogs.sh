68bcdb7034e7ab77ac0ae8835aee2c3bbd24bbc8
rakesh <rakeshreddy@gmail.com>
2025-06-22 14:08:22 +0200

    added deletinglogs.sh

--- deletinglogs.sh
+++ deletinglogs.sh
@@ -6,11 +6,11 @@ scriptname=$0
 LOGS_DIR=/home/ec2-user/ssh-logs
 LOGFILE=$LOGS_DIR/$scriptname-$DATE.log
 userid=$(id -u)
-if [ $userid -ne 0 ]
-then
-echo "user has no root access. proceed with root access"
-exit 1
-fi
+# if [ $userid -ne 0 ]
+# then
+# echo "user has no root access. proceed with root access"
+# exit 1
+# fi
 
 FILES_TO_DELETE=$( find $applogs -name "*.log" -mtime +14)
 echo "$LOGFILE"
