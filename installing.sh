89249bd6b0cc832020cb1f52b35c98fedb8e725e
rakesh <rakeshreddy@gmail.com>
2025-06-22 13:11:15 +0200

    updated installation.sh

--- installing.sh
+++ installing.sh
@@ -8,10 +8,28 @@ R="\e[0;31m"
 G="\e[0;32m"
 Y="\e[0;33m"
 N="\e[0m"
+validate(){
+if [ $1 -ne 0 ]
+echo  -e " $2  $R installation....failure $N"
+else
+echo -e " $2  $G installation....success $N"
+
+fi
+}
 userid=$(id -u)
 if [ $userid -ne 0 ]
 then
 echo "user has no root access. proceed with root access"
 exit 1
 fi
-echo -e "$LOGFILE $R $G $Y $N"
+for i in $@
+do
+yum list installed $i &>>$LOGFILE
+if [ $? -ne 0 ]
+then 
+echo -e " $i not installed proceed for installation"
+sudo yum install $i -y &>>$LOGFILE
+validate $? $i
+else 
+echo  -e " $i  $Y installed already $N"
+done
