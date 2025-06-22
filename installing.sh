ee922900b17727c3a6c1e8dd8dbc7695909f53a4
rakesh <rakeshreddy@gmail.com>
2025-06-22 12:47:55 +0200

    updated installation.sh

--- installing.sh
+++ installing.sh
@@ -4,10 +4,14 @@ LOGS_DIR=/home/ec2-user/ssh-logs
 DATE=$(date +%F-%H-%M-%S)
 scriptname=$0
 LOGFILE=$LOGS_DIR/$scriptname-$DATE.log
+R="\e[0;31m"
+G="\e[0;32m"
+Y="\e[0;33m"
+N="\e[0m"
 userid=$(id -u)
 if [ $userid -ne 0 ]
 then
 echo "user has no root access. proceed with root access"
 exit 1
 fi
-echo "$LOGFILE"
+echo "$LOGFILE $R $G $Y $N"
