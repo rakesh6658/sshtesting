1dfb31cb8c7f426babc57f34c665a2c434367f2e
rakesh <rakeshreddy@gmail.com>
2025-06-24 12:12:47 +0200

    added diskuasge.sh

--- diskusage.sh
+++ diskusage.sh
@@ -3,5 +3,18 @@
 threshold=1
 
 disk_usage=$(df -hT | grep -vE 'tmpfs|Filesystem|Root')
+message=
 
 echo "$disk_usage"
+
+while IFS= read line
+do
+usage=$(echo $line | awk '{print $6}' | cut -d '%' -f 1 )
+partition=$(echo $line | awk '{print $1}')
+if [ $usage -gt $partition ]
+then
+message+="High disk usage on $partition : $usage"
+
+
+done <<< $disk_usage
+echo "$message"
